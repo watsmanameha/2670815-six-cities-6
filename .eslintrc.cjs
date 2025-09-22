27f3d67bf7c71e93a119656d1fccf80e0c569040
Eugene July <dzhulay.evgeniy@gmail.com>
2025-09-22 22:24:35 +0300

    style: npx prettier --write .

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -6,10 +6,14 @@ module.exports = {
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:react-hooks/recommended',
-    "htmlacademy/react-typescript",
+    'htmlacademy/react-typescript',
   ],
   parser: '@typescript-eslint/parser',
-  parserOptions: { ecmaVersion: 'latest', sourceType: 'module', project: 'tsconfig.json' },
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    project: 'tsconfig.json',
+  },
   settings: { react: { version: 'detect' } },
   plugins: ['react-refresh'],
   rules: {
@@ -17,8 +21,8 @@ module.exports = {
   },
   overrides: [
     {
-      files: [ '*test*' ],
-      rules: { '@typescript-eslint/unbound-method': 'off' }
+      files: ['*test*'],
+      rules: { '@typescript-eslint/unbound-method': 'off' },
     },
   ],
-}
+};
