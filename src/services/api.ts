97a3c1cff0c0720b4c9a60897e06049a714f6e5c
Eugene July <62775429+watsmanameha@users.noreply.github.com>
2025-11-29 02:44:56 +0300

    Создан модуль api.ts с конфигурацией axios

---
+++ src/services/api.ts
@@ -0,0 +1,13 @@
+import axios, { AxiosInstance } from 'axios';
+
+const BASE_URL = 'https://14.design.htmlacademy.pro/six-cities';
+const REQUEST_TIMEOUT = 5000;
+
+export const createAPI = (): AxiosInstance => {
+  const api = axios.create({
+    baseURL: BASE_URL,
+    timeout: REQUEST_TIMEOUT,
+  });
+
+  return api;
+};
